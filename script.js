82580403962c3b1c50aebb116d512423bc77ec0a
wdkowd <wdkowd@gmail.com>
2026-06-02 09:15:32 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://tomato-laugh-accordingly-bacon.trycloudflare.com";
+const API_URL = "https://prospect-knowing-fitted-physicians.trycloudflare.com";
     // "http://localhost:8000";
 
 let lastUpdate = 0;
