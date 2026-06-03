c9412d30beb7b2437a4a5bc285a81d65a61be336
wdkowd <wdkowd@gmail.com>
2026-06-03 09:57:05 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://relying-kernel-principle-suddenly.trycloudflare.com";
+const API_URL = "https://beings-ips-copies-declared.trycloudflare.com";
     // "http://localhost:8000";
 
 let lastUpdate = 0;
