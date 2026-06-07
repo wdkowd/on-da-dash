6116c20e8fe4706c24ae4b18bda10ecc85c54443
wdkowd <wdkowd@gmail.com>
2026-06-07 19:40:18 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://conducted-views-immunology-formal.trycloudflare.com";
+const API_URL = "https://gadgets-prophet-later-print.trycloudflare.com";
     // "http://localhost:8000";
 
 let lastUpdate = 0;
