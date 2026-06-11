e997ca114ec762d11b29c8db937a0dd71a6ec4d4
wdkowd <wdkowd@gmail.com>
2026-06-11 13:00:40 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://innocent-blades-louise-legacy.trycloudflare.com";
+const API_URL = "https://collect-gentleman-qld-after.trycloudflare.com";
 
 let lastUpdate = 0;
 let currentTabFile = null;
