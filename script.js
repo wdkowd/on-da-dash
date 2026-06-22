d3e2c037567cf425b9d702045b9c64ca8c2f7583
wdkowd <wdkowd@gmail.com>
2026-06-22 09:16:00 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://ongoing-andreas-request-illustration.trycloudflare.com";
+const API_URL = "https://recycling-allocation-curve-convenient.trycloudflare.com";
 
 let lastUpdate = 0;
 let currentTabFile = null;
