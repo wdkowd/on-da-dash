5f3f09bae90d1ba92752623e72b7958d0e9e8e79
wdkowd <wdkowd@gmail.com>
2026-06-29 09:37:30 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://ability-dealing-format-juice.trycloudflare.com";
+const API_URL = "https://puerto-experts-publicity-receiving.trycloudflare.com";
 
 let lastUpdate = 0;
 let currentTabFile = null;
