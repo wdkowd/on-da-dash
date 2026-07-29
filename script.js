a41446e8466555a3553aab32d222b4627367462b
wdkowd <wdkowd@gmail.com>
2026-07-29 12:21:43 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://forget-lauderdale-simulations-hiking.trycloudflare.com";
+const API_URL = "https://someone-movements-definitions-pills.trycloudflare.com";
 
 let lastUpdate = 0;
 let currentTabFile = null;
