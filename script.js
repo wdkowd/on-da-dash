03f765f6e27a2ac8350578832ab06668e358f7c6
wdkowd <wdkowd@gmail.com>
2026-09-09 03:59:47 -0400

    Update tunnel URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://build-courtesy-treasury-debug.trycloudflare.com";
+const API_URL = "https://caused-parent-bandwidth-kansas.trycloudflare.com";
 
 let lastUpdate = 0;
 let currentTabFile = null;
